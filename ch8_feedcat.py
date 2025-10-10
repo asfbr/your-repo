0663c807db2ea1479c307c74b691edbe1f963caa
asfbr <andresfontoura@gmail.com>
2025-10-10 18:38:11 -0300

    Chapter 8

---
+++ ch8_feedcat.py
@@ -0,0 +1,31 @@
+
+    # spam = 'Say hi to Bob\'s mother.'
+    # print(spam)
+
+#print(r'The file is in C:\Users\Alice\Desktop')
+
+#print('''Dear Alice,
+
+#Can you feed Eve's cat this weekend?
+
+#Sincerely,
+#Bob''')
+
+"""This is a test Python program.
+Written by Al Sweigart al@inventwithpython.com
+
+This program was designed for Python 3, not Python 2.
+"""
+
+def say_hello():
+    """This function prints hello.
+    It does not return anything."""
+    print('Hello!')
+
+say_hello()
+
+
+name = 'Al'
+age = 4000
+print(f'My name is {name}. I am {age} years old.')
+print(f'In ten years I will be {age + 10}')
